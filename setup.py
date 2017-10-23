25f5af1f644377fb009ee49984a13e0bf09bcf7b
Sho Mizoe <sho.mizoe@gmail.com>
2017-10-23 09:13:18 -0700

    added information to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,20 @@
 from distutils.core import setup
-
 setup(
     name='qrnr',
-    version='0.1',
+    version='0.1.0',
     packages=['qrnr'],
+    author='Sho Mizoe',
+    author_email='sho.mizoe@gmail.com',
+    url='https://github.com/smizoe/qrnr',
+    python_requires='>=3',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+        'Topic :: Database :: Database Engines/Servers',
+    ],
     entry_points={
         'console_scripts': [
             'qrnr = qrnr.__main__:main'
